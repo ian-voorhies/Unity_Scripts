a31a72c83a77c36afda585b808c6c8dd5917a4b8
Ian Voorhies <33384413+ian-voorhies@users.noreply.github.com>
2018-03-20 12:55:21 -0700

    Create SwitchScene.cs

---
+++ SceneControl/SwitchScene.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SwitchScene:MonoBehaviour{
+    public bool enable=true,auto=true;
+    public string scene;
+    public float delay=0.0f;
+    private bool delayed=true,done=false;
+    private float time=0.0f;
+    void Start(){if(auto)go();}
+	  void Update(){
+		    if(!enable)return;
+		    if(!delaying()&&!done)onUpdate();
+	  }
+    public void go(){
+        if(!enable)return;
+        if(delay>0)delayed=false;
+        else onGo();
+    }
+    private void onGo(){switchScene(scene);}
+	  private void onUpdate(){switchScene(scene);}
+    private void switchScene(string scene){
+        SceneManager.LoadScene(scene);
+        done=true;
+    }
+    private bool delaying(){
+        if(delayed)return false;
+        time+=Time.deltaTime;
+        if(time>=delay){
+            time=0.0f;
+            delayed=true;
+        }
+        return true;
+    }
+}
